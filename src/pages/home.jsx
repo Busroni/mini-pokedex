c56a2e127a77a85c963a1c38838ab047033d16ec
Busroni <busroni10@gmail.com>
2025-02-21 23:53:03 +0700

    change list to grid

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchPokemonSpecies } from "../services/api";
+import PokeCard from "../components/PokeCard"
 
 const Home = () => {
   const [data, setData] = useState(null);
@@ -22,13 +23,15 @@ const Home = () => {
   if (!data) return <div className="text-center">Loading...</div>;
 
   return (
-    <div className="p-5">
-      <h1 className="text-5xl font-bold mb-4">Pokémon Species</h1>
-      <ul>
-        {data.results?.map((pokemon, index) => (
-          <li key={index} className="mb-2">{pokemon.name}</li>
-        ))}
-      </ul>
+    <div className="p-2">
+      <h1 className="text-5xl font-bold mb-4 p-5 rounded-lg bg-sky-800 text-amber-50">Pokémon Species</h1>
+
+          <div className="grid grid-cols-3 md:grid-cols-4 gap-2">
+          {data.results?.map((pokemon, index) => ( 
+          <PokeCard key={index} pokemonId={pokemon.name} />
+          ))}     
+
+      </div>
     </div>
   );
 };
