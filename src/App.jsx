8a955d47370f313647db959499d1f400ab722bf2
Busroni <busroni10@gmail.com>
2025-02-21 23:52:05 +0700

    app fix element

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import ErrorBoundary from "./components/ErrorBoundary";
 const App = () => {
   return (
     <ErrorBoundary>
-      <p className="bg-lime-300 p-50">APA inni</p>
       <AppRouter />
     </ErrorBoundary>  
     
