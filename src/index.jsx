a03bfa72b3e107673fff6d5f1d8414b83641dbbb
Busroni <busroni10@gmail.com>
2025-02-24 23:33:58 +0700

    add Reactdom Footer

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import SearchBar from "./components/SearchBar";
+import Footer from "./components/Footer";
 
 import App from "./App";
 // import style
@@ -15,3 +16,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 
 // Render SearchBar ke #search-container
 ReactDOM.createRoot(document.getElementById("search-container")).render(<SearchBar />);
+ReactDOM.createRoot(document.getElementById("footer")).render(<Footer />);
