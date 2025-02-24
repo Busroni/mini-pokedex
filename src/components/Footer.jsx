6e763a8222c670ff7902ce9bdc5809f6ff6c7ffc
Busroni <busroni10@gmail.com>
2025-02-24 23:34:29 +0700

    add Footer as components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Footer extends Component{
+
+    render() {
+        return (
+            <div className="bg-slate-950 text-white text-center p-5">
+            <h1>&#169; 2025-Busroni Ahmad Safrizal</h1>
+            </div>
+        )
+    }
+}
+
+export default Footer;
