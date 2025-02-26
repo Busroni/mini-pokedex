566904881d4d8f14995cd5576ed0faeaf2ad176e
Busroni <busroni10@gmail.com>
2025-02-26 23:46:10 +0700

    detail component for pokemon information

---
+++ src/components/Detail.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Detail extends Component {
+
+    render() {
+        return (
+
+            <div>details pokemon</div>
+
+        );
+    };
+
+}
+
+export default Detail;
