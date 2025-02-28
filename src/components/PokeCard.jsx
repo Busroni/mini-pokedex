d35264a34b72d1f6f8051e292a89eee2160ce10e
Busroni <busroni10@gmail.com>
2025-02-28 22:27:43 +0700

    update link target card

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -36,7 +36,7 @@ const PokeCard = ({ pokemonId }) => {
         Height: {pokemon.height} | Weight: {pokemon.weight}
       </p>
       <a
-        href={`https://www.pokemon.com/us/pokedex/${pokemon.name}`}
+        href={`/pokemon/${pokemon.name}`}
         target="_blank"
         rel="noopener noreferrer"
         className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
