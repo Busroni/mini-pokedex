2c013622e6b8b1b48b808c1d2edf526ca94d3284
Busroni <busroni10@gmail.com>
2025-03-04 23:13:56 +0700

    change stat to progress bar

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -51,30 +51,44 @@ const Detail = ({ pokemonId }) => {
     </div>
     
     <div className="p-10 bg-slate-700 mt-3 rounded-lg text-white">
-      <h1 className="text-4xl font-bold text-orange-300 mb-2">Stat Pokemon</h1>
-      <p className="mb-2 text-amber-500  text-2xl font-bold font-mono">
-          Base Experience: {pokemon.base_experience}
-        </p>
-        <div>
+      <div className="text-4xl font-bold text-orange-300 mb-2 capitalize">Stat {pokemon.name} 
           {pokemon.types.map((item, index) => (
-            <span
-              key={index}
-              className={`text-white rounded-lg px-3 py-1 mr-2 ${getColor(item.type.name)}`}
-            >
-              {item.type.name.charAt(0).toUpperCase() + item.type.name.slice(1)}
-            </span>
-          ))}
+                <span
+                  key={index}
+                  className={`text-white text-2xl rounded-lg px-3 py-1 mr-2 ml-3 ${getColor(item.type.name)}`}
+                >
+                  {item.type.name.charAt(0).toUpperCase() + item.type.name.slice(1)}
+                </span>
+              ))}
+      </div>
+      <p className="mb-2 text-amber-300  text-2xl font-bold font-mono">
+          Base Experience: {pokemon.base_experience}
+      </p>
+      <div className="grid grid-cols-3 gap-7">
+  <div>
+    {pokemon.stats.map((item, index) => (
+      <div key={index}>
+        <div className="flex justify-between mb-1">
+          <span className="text-lg font-medium text-blue-700 dark:text-white">{item.stat.name.charAt(0).toUpperCase() + item.stat.name.slice(1)}</span>
+          <span className="text-lg font-medium text-blue-700 dark:text-white">{item.base_stat}</span>
         </div>
-        <div>
-          {pokemon.stats.map((item, index) => (
-              <p key={index}>
-                {item.stat.name.charAt(0).toUpperCase() + item.stat.name.slice(1)} : {item.base_stat}
-              </p>
-            ))}
+        <div className="w-full bg-gray-200 rounded-full h-3 dark:bg-gray-600">
+          <div className="bg-lime-300 h-3 rounded-full" style={{ width: `${item.base_stat * 100 / 255}%` }}></div>
         </div>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Height: {pokemon.height*10} CM | Weight: {pokemon.weight*0.1} KG
-        </p>
+      </div>
+    ))}
+  </div>
+  <div className="text-gray-700 text-xl dark:text-gray-400 text-center gap-4 justify-center">
+    <div>Height<p className="text-6xl text-amber-50 font-bold">{pokemon.height * 10} CM </p></div>
+    --- --- ---
+    <div>Weight<p className="text-6xl text-amber-50 font-bold">{pokemon.weight * 0.1} KG</p></div>
+    
+  </div>
+  <div className="text-gray-700 dark:text-gray-400 text-center flex items-center justify-center">
+    Weight: {pokemon.weight * 0.1} KG
+  </div>
+</div>
+
     </div>
     </div>
   );
